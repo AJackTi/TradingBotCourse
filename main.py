911d0b90b26824e5829bf4456db30149ae8cf519
AJackTi <dtrong97vn@gmail.com>
2022-03-03 14:09:20 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s :: %(message)s')
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+# logger.debug("This message is important only when debugging the program")
+# logger.info("This message just shows basic information")
+# logger.warning("This message is about something you should pay attention to")
+# logger.error("This message helps to debug an error that occurred in your program")
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.mainloop()
