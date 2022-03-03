8c827b00e762b806d77afc854b9f9927f6353c38
AJackTi <dtrong97vn@gmail.com>
2022-03-03 14:41:51 +0700

    Interacting with a public API

--- binance_futures.py
+++ binance_futures.py
@@ -1,4 +1,24 @@
 import logging
+import requests
+import pprint
 
 logger = logging.getLogger()
 
+
+def get_contracts():
+    response_object = requests.get("https://fapi.binance.com/fapi/v1/exchangeInfo")
+    # print(response_object.status_code)
+    # pprint.pprint(response_object.json())
+
+    # pprint.pprint(response_object.json()['symbols'])
+
+    contracts = []
+
+    for contract in response_object.json()['symbols']:
+        # pprint.pprint(contract)
+        # print(contract['pair'])
+        contracts.append(contract['pair'])
+
+    return contracts
+
+print(get_contracts())
